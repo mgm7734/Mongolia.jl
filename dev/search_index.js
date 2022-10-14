1eb752d57a3ea5db64513f6b6627b784835ace8d
Documenter.jl <documenter@juliadocs.github.io>
2022-10-14 02:06:52 +0000

    build based on 3982a57

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Mongolia","category":"page"},{"location":"#Mongolia","page":"Home","title":"Mongolia","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Mongolia.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Mongolia]","category":"page"}]
+}
